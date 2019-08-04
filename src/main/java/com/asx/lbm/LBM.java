d49ab6d282d24787da056f8b7175e487ab3a179a
Ri5ux <Ri5ux>
2019-08-03 20:19:47 -0500

    Initial project setup.

---
+++ src/main/java/com/asx/lbm/LBM.java
@@ -0,0 +1,26 @@
+package com.asx.lbm;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = LBM.MODID, name = LBM.NAME, version = LBM.VERSION)
+public class LBM
+{
+    public static final String MODID = "lbm";
+    public static final String NAME = "Lightweight Blood Mechanics";
+    public static final String VERSION = "1.0";
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        ;
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        ;
+    }
+}
